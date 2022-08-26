197f47602270b839f4842e22e0d3e3d550f9fe1e
SHIMAD4 <6asm6deus6@gmail.com>
2022-08-26 22:44:49 +0300

    added js

---
+++ js/card-change.js
@@ -0,0 +1,51 @@
+let $cardNumber_prg = document.querySelector('.card-number-prg')       // Забрал параграф с номером карты
+let $cardName_prg   = document.querySelector('.card-name-prg')         // Забрал параграф с именем пользователя
+let $cardDateMM_prg = document.querySelector('.card-mm-prg')           // Забрал параграф с месяцем
+let $cardDateYY_prg = document.querySelector('.card-yy-prg')           // Забрал параграф с годом
+let $cardСvc_prg    = document.querySelector('.card-cvc-prg')          // Забрал параграф с CvC
+
+
+let cardNumber = document.querySelector('.card-number')
+cardNumber.oninput = function() {
+  $cardNumber_prg.innerHTML = cardNumber.value;
+
+  if(cardNumber.value === ''){
+    $cardNumber_prg.innerHTML = '0000 0000 0000 0000'
+  }
+}
+
+let userName = document.querySelector('.user-name')
+userName.oninput = function() {
+  $cardName_prg.innerHTML = userName.value;
+
+  if(userName.value === ''){
+    $cardName_prg.innerHTML = 'Cardholder Name'
+  }
+}
+
+let cardDateMM = document.querySelector('.card_mm')
+cardDateMM.oninput = function() {
+  $cardDateMM_prg.innerHTML = cardDateMM.value;
+
+  if(cardDateMM.value === ''){
+    $cardDateMM_prg.innerHTML = '00'
+  }
+}
+
+let cardDateYY = document.querySelector('.card_yy')
+cardDateYY.oninput = function() {
+  $cardDateYY_prg.innerHTML = cardDateYY.value;
+
+  if(cardDateYY.value === ''){
+    $cardDateYY_prg.innerHTML = '00'
+  }
+}
+
+let cardСvc = document.querySelector('.card_cvc')
+cardСvc.oninput = function() {
+  $cardСvc_prg.innerHTML = cardСvc.value;
+
+  if(cardСvc.value === ''){
+    $cardСvc_prg.innerHTML = '000'
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const mask = (dataValue, options) => {                                          // создаем универсальную функцию
+    const elements = document.querySelectorAll(`[data-mask="${dataValue}"]`)      // ищем поля ввода по селектору с переданным значением data-атрибута
+    if (!elements) return                                                         // если таких полей ввода нет, прерываем функцию
+
+    elements.forEach(el => {                                                      // для каждого из полей ввода
+      IMask(el, options)                                                          // инициализируем плагин imask для необходимых полей ввода с переданными параметрами маски
+    })
+  }
+
+  mask('card-number', {
+    mask: '0000 0000 0000 0000',
+    max: 19
+  })
+
+  mask('card-mm', {
+    mask: '00'
+  })
+
+  mask('card-yy', {
+    mask: '00'
+  })
+
+  mask('card-cvc', {
+    mask: '000'
+  })
+})
